82fc8f76daebdf418263400cfd72e2817935fe79
Luca Nioi <lucahnioi@gmail.com>
2023-11-28 14:31:54 +0900

    Update backlog

--- spec/chatbot/commands/spent_spec.rb
+++ spec/chatbot/commands/spent_spec.rb
@@ -1,15 +1,15 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Chatbot::Commands::SetBudget do
-#   it_behaves_like 'command', {
-#     'basic' => {
-#       input: 'spent 10.32 food',
-#       output: 'Spent €1,000',
-#     },
-#   }
+RSpec.describe Chatbot::Commands::SetBudget do
+  it_behaves_like 'command', {
+    'basic' => {
+      input: 'spent 10.32 food',
+      output: 'Spent €1,000',
+    },
+  }
 
-#   # describe 'state changes' do
-#   #   let(:result) { described_class.new(message).execute }
-#   #   let(:message) { 'spent 5.50 food' }
-#   # end
-# end
+  # describe 'state changes' do
+  #   let(:result) { described_class.new(message).execute }
+  #   let(:message) { 'spent 5.50 food' }
+  # end
+end
