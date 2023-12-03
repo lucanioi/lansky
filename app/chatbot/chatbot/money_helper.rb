3666ebbcb64b6c4b816451f75e24a78b9e59979b
Luca Nioi <lucahnioi@gmail.com>
2023-12-03 12:26:54 +0100

    Enable currencies to be specified in money helper

--- app/chatbot/chatbot/money_helper.rb
+++ app/chatbot/chatbot/money_helper.rb
@@ -1,25 +1,32 @@
 module Chatbot
   module MoneyHelper
     MONEY_MATCHER = /^(€|$)?\d{1,3}(?:,?\d{3})*(?:\.\d{1,2})?$/
+    DEFAULT_CURRENCY = :eur
+    CURRENCY_SYMBOLS = {
+      eur: '€'
+    }.freeze
 
     module_function
 
-    def format_euros(amount_in_cents)
-      euros = amount_in_cents / 100
+    def format_euros(amount_in_cents, currency: :eur)
+      euros  = amount_in_cents / 100
       amount = euros.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
-      "€#{amount}"
+      "#{currency_symbol(currency)}#{amount}"
     end
 
-    def format_euros_with_cents(amount_in_cents)
-      euros = amount_in_cents / 100
-      cents = amount_in_cents % 100
+    def format_euros_with_cents(amount_in_cents, currency: :eur)
+      euros  = amount_in_cents / 100
+      cents  = amount_in_cents % 100
       amount = euros.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+      currency_symbol = currency_symbol(currency)
 
-      cents > 0 ? "€#{amount}.#{pad(cents)}" : "€#{amount}"
+      cents > 0 ? "#{currency_symbol}#{amount}.#{pad(cents)}" : "#{currency_symbol}#{amount}"
     end
 
-    def parse_to_cents(money_string)
-      money_string.gsub('€', '').gsub(',', '').to_f * 100
+    def parse_to_cents(money_string, currency: :eur)
+      currency_symbol = currency_symbol(currency)
+
+      money_string.gsub(currency_symbol, '').gsub(',', '').to_f * 100
     end
 
     def parse_amount(string)
@@ -32,6 +39,10 @@ module Chatbot
 
     def pad(amount)
       amount.to_s.rjust(2, '0')
+    end
+
+    def currency_symbol(currency_key)
+      CURRENCY_SYMBOLS[currency_key] || CURRENCY_SYMBOLS[DEFAULT_CURRENCY]
     end
   end
 end
