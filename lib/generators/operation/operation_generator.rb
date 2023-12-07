8b0231fac8b471d58e172f663a0f85352c95e44b
Luca Nioi <lucahnioi@gmail.com>
2023-12-07 12:44:35 +0100

    Create Operation generator

---
+++ lib/generators/operation/operation_generator.rb
@@ -0,0 +1,24 @@
+require 'rails/generators'
+
+class OperationGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('templates', __dir__)
+
+  argument :params, type: :array, default: [], banner: "param1 param2 param3"
+
+  def create_operation_file
+    template 'operation.erb',
+             File.join("app/chatbot/chatbot/operations", "#{file_name}.rb")
+  end
+
+  def create_operation_spec_file
+    template 'operation_spec.erb',
+             File.join('spec/chatbot/operations', "#{file_name}_spec.rb")
+  end
+
+  def create_params_file
+    return if params.empty?
+
+    template 'params.erb',
+             File.join("app/chatbot/chatbot/params", "#{file_name}.rb")
+  end
+end
