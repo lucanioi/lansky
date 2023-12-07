cb614f24094a9c6990e079c2e07a7ecb932dbadf
Luca Nioi <lucahnioi@gmail.com>
2023-12-07 15:52:00 +0100

    Fix issue with generator

--- lib/generators/operation/operation_generator.rb
+++ lib/generators/operation/operation_generator.rb
@@ -1,28 +1,24 @@
 require 'rails/generators'
 
-module Generators
-  module Operation
-    class OperationGenerator < Rails::Generators::NamedBase
-      source_root File.expand_path('templates', __dir__)
+class OperationGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('templates', __dir__)
 
-      argument :params, type: :array, default: [], banner: "param1 param2 param3"
+  argument :params, type: :array, default: [], banner: "param1 param2 param3"
 
-      def create_operation_file
-        template 'operation.erb',
-                File.join("app/chatbot/chatbot/operations", "#{file_name}.rb")
-      end
+  def create_operation_file
+    template 'operation.erb',
+            File.join("app/chatbot/chatbot/operations", "#{file_name}.rb")
+  end
 
-      def create_operation_spec_file
-        template 'operation_spec.erb',
-                File.join('spec/chatbot/operations', "#{file_name}_spec.rb")
-      end
+  def create_operation_spec_file
+    template 'operation_spec.erb',
+            File.join('spec/chatbot/operations', "#{file_name}_spec.rb")
+  end
 
-      def create_params_file
-        return if params.empty?
+  def create_params_file
+    return if params.empty?
 
-        template 'params.erb',
-                File.join("app/chatbot/chatbot/params", "#{file_name}.rb")
-      end
-    end
+    template 'params.erb',
+            File.join("app/chatbot/chatbot/params", "#{file_name}.rb")
   end
 end
