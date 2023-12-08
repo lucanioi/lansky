c47da13df270e00d0df692a200cd4dd601c57cf2
Luca Nioi <lucahnioi@gmail.com>
2023-12-08 17:56:30 +0100

    create period class

--- app/chatbot/chatbot/models/period.rb
+++ app/chatbot/chatbot/models/period.rb
@@ -1,4 +1,57 @@
 module Chatbot
   module Models
+    class Period
+      attr_reader :period_start, :period_end
+
+      DAY_DURATION   = (1.day - 1.seconds).to_f..1.day.to_f
+      WEEK_DURATION  = (1.week - 1.seconds).to_f..1.week.to_f
+      MONTH_DURATION = (28.days - 1.second).to_f..31.days.to_f
+      YEAR_DURATION  = (1.year - 1.seconds).to_f..1.year.to_f
+
+      def initialize(period_start:, period_end:)
+        @period_start = period_start
+        @period_end = period_end
+      end
+
+      def range
+        period_start..period_end
+      end
+
+      def duration
+        period_end - period_start.to_time
+      end
+
+      def day?
+        DAY_DURATION.cover?(duration)
+      end
+
+      def week?
+        WEEK_DURATION.cover?(duration)
+      end
+
+      def month?
+        MONTH_DURATION.cover?(duration)
+      end
+
+      def year?
+        YEAR_DURATION.cover?(duration)
+      end
+
+      def calendar_day?
+        day? && period_start.hour.zero? && period_start.min.zero?
+      end
+
+      def calendar_week?
+        week? && period_start.wday == 1
+      end
+
+      def calendar_month?
+        month? && period_start.day == 1
+      end
+
+      def calendar_year?
+        year? && period_start.day == 1 && period_start.month == 1
+      end
+    end
   end
 end
